2f1c9468fe583a3df3e706ca097ce3db59277563
basheer mohammad <51733433+basheerali@users.noreply.github.com>
2021-10-02 16:51:50 +0530

    add some code

---
+++ conf.js
@@ -0,0 +1,6 @@
+setChartLibrary('google-chart');
+setChartTitle('Water Level Monitoring !!!');
+setChartType('areaGraph');
+setAxisName('Time Stamp', 'Water Level');
+plotChart('time_stamp', 'temp');
+//temp is the variable name
